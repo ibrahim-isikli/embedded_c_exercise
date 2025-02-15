b57a9220d21a22b9acef43134244f70c853a93cf
ibrahim ışıklı <isikli.ibrahim@outlook.com>
2025-02-15 22:08:51 +0300

    ADD- pointer to pointer exercise 4

---
+++ 009_pointer_to_pointer/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void allocateMemory(int **ptr)
+{
+    *ptr = (int*)malloc(sizeof(int)); // allocate dynamic memory
+    if(*ptr == NULL)
+    {
+        printf("memory not allocated");
+        exit(1);
+    }
+    **ptr = 99;
+}
+
+
+int main(void)
+{
+    int *p = NULL;
+    allocateMemory(&p);
+    printf("value stored in dynamic memory: %d\n",*p);
+}
