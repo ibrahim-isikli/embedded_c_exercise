c4ef6aab2cc81bec0f7d691ab77197f5d9151b8b
ibrahim ışıklı <isikli.ibrahim@outlook.com>
2025-02-15 19:27:54 +0300

    ADD- pointer to pointer exercise 3

---
+++ 008_pointer_to_pointer/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+int changePointer(int **ptr)
+{
+    **ptr = 42;
+}
+
+int main(void)
+{
+    int num = 10;
+    int *ptr = &num;
+    int **pp = &ptr;
+
+    printf("address of num = %d\tvalue of num= %d\n",&num,num);
+    changePointer(pp);
+    printf("address of num = %d\tvalue of num= %d\n",&num,num);
+
+}
