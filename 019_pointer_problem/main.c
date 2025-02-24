34a94df913cd519331031edf951445bc9768fe04
ibrahim ışıklı <isikli.ibrahim@outlook.com>
2025-02-24 23:58:33 +0300

    ADD- pointer problem 2

---
+++ 019_pointer_problem/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+
+
+int main(void)
+{
+    int8_t size,min_val,max_val;
+    printf("array size:\t");
+    scanf("%d",&size);
+    int8_t *arr = (int8_t*)malloc(sizeof(uint8_t)*size);
+
+    for(int8_t i=0; i<size; i++)
+    {
+        printf("[%d]:\t");
+        scanf("%d",arr+i);
+    }
+
+
+    *arr = min_val = max_val;
+
+    for(int8_t i=1; i<size; i++)
+    {
+        if(*(arr+i) < min_val)
+        {
+            min_val = *(arr+i);
+        }
+
+        if(*(arr+i) > max_val)
+        {
+            max_val = *(arr+i);
+        }
+    }
+
+
+    printf("min_val=%d",min_val);
+    printf("max_val=%d",max_val);
+
+    
+
+
+    return 0;
+}
