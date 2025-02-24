bce5303d46a5b65814349977561094d8c5638614
ibrahim ışıklı <isikli.ibrahim@outlook.com>
2025-02-25 00:20:39 +0300

    FIX- pointer problem 2

--- 019_pointer_problem/main.c
+++ 019_pointer_problem/main.c
@@ -8,17 +8,16 @@ int main(void)
 {
     int8_t size,min_val,max_val;
     printf("array size:\t");
-    scanf("%d",&size);
-    int8_t *arr = (int8_t*)malloc(sizeof(uint8_t)*size);
+    scanf("%hhd", &size);
+    int8_t *arr = (int8_t*)malloc(sizeof(int8_t)*size);
 
     for(int8_t i=0; i<size; i++)
     {
         printf("[%d]:\t",i);
-        scanf("%d",arr+i);
+        scanf("%hhd", arr + i);
     }
 
-
-    *arr = min_val = max_val;
+    min_val = max_val = *arr;
 
     for(int8_t i=1; i<size; i++)
     {
@@ -34,10 +33,10 @@ int main(void)
     }
 
 
-    printf("min_val=%d",min_val);
-    printf("max_val=%d",max_val);
+    printf("min_val=%d\n",min_val);
+    printf("max_val=%d\n",max_val);
 
-    
+    free(arr);
 
 
     return 0;
