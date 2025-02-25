ceccd870e2a798fc8ee8495510143799d758ccca
ibrahim ışıklı <isikli.ibrahim@outlook.com>
2025-02-25 23:27:55 +0300

    FIX- pointer problem 4

--- 021_pointer_problem/main.c
+++ 021_pointer_problem/main.c
@@ -1,56 +1,90 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    int num_students; 
 
-    printf("ogrenci sayisini girin: ");
-    scanf("%d", &num_students);
+/*
+    THE STRUCTURE
 
-    // ogrencilerin notlarini tutacak dinamik bellek tahsisi
-    int **grades = (int**)malloc(num_students * sizeof(int*));
-    int *exam_count = (int*)malloc(num_students * sizeof(int)); // Her ogrencinin kac sinavi var
+       PTP         PTP        PTP                      MAIN POINTER (POINTER TO POINTER)
+        |           |          |                       (SPACE NUM_STUDENTS) 
+    [STUDENT 1] [STUDENT 2] [STUDENT 3]                
+        |           |          |                       (SPACE EXAM COUNT)
+      [EXAM1]    [EXAM1]     [EXAM1]
+      [EXAM2]    [EXAM2]     [EXAM2]
+      [EXAM3]                [EXAM3]
+                             [EXAM4]
 
-    if (grades == NULL || exam_count == NULL) {
-        printf("Bellek tahsisi basarisiz!\n");
-        return 1;
+
+
+    PTP[i] -> student
+    PTP[i][j] -> student's grade
+*/
+
+
+int main(void) 
+{
+
+    int num_students;
+
+    printf("enter the num of students:\t");
+    scanf("%d",&num_students); // example num_students = 3
+
+    int **grades = (int**)malloc(num_students*sizeof(int*));  // 3 ptp [student 1] [student 2] [student 3]
+    int *exam_count =  (int*)malloc(num_students*sizeof(int));  // Has [student 1] How many exam ?
+
+    if((grades == NULL) || (exam_count == NULL))
+    {
+        printf("mem allocation failed!\n");
+        return -1;
     }
 
-    // Her ogrenci icin sinav sayisini ve notlarini al
-    for (int i = 0; i < num_students; i++) {
-        printf("ogrenci %d kac sinava girdi? ", i + 1);
-        scanf("%d", &exam_count[i]);
+    // get exam_count and grades for each studens
+    for(int i=0; i<num_students; i++)
+    {
+        printf("How many exams did the student take ?\n"); 
+        scanf("%d",&exam_count[i]);
+        // memory allocate up to number of exams
+        grades[i] = (int*)malloc(exam_count[i]*sizeof(int));
 
-        grades[i] = (int*)malloc(exam_count[i] * sizeof(int)); // Sinav sayisi kadar bellek tahsis et
-
-        if (grades[i] == NULL) {
-            printf("Bellek tahsisi basarisiz!\n");
-            return 1;
+        if(grades[i] == NULL)
+        {
+            printf("memory allocation failed!\n");
+            return -1;
         }
 
-        printf("ogrenci %d icin sinav notlarini girin:\n", i + 1);
-        for (int j = 0; j < exam_count[i]; j++) {
-            printf("Not %d: ", j + 1);
-            scanf("%d", &grades[i][j]);
+        // enter the grades for each students
+        for(int j=0; j<exam_count[i]; j++)
+        {
+            printf("|Student %d| Grade[%d] |\t",i+1,j+1);
+            scanf("%d",&grades[i][j]);
         }
+
     }
 
-    // Ortalama hesaplama ve ekrana yazdirma
-    printf("\nogrencilerin not ortalamalari:\n");
-    for (int i = 0; i < num_students; i++) {
+    // calculete average of grades for each student
+    printf("\naverage of grades\n");
+    for(int i=0; i<num_students; i++)
+    {
         int sum = 0;
-        for (int j = 0; j < exam_count[i]; j++) {
-            sum += grades[i][j];
+        for(int j=0; j<exam_count[i]; j++)
+        {
+            sum+=grades[i][j];
         }
-        printf("ogrenci %d: %.2f\n", i + 1, (double)sum / exam_count[i]);
+        printf("|Student %d| Average[%.2f] |\t",i+1,(double)sum/exam_count[i]);
+
+    }
+    
+
+    // free memory
+
+    for(int i=0; i<num_students; i++)
+    {
+        free(grades[i]);
     }
 
-    // Bellegi temizleme
-    for (int i = 0; i < num_students; i++) {
-        free(grades[i]); // Her ogrencinin notlarini iceren diziyi temizle
-    }
     free(grades);
     free(exam_count);
+
 
     return 0;
 }
