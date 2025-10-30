58b6815faf8f1aa2566a1cb73468de7429147f3c
ibrahim ışıklı <isikli.ibrahim@outlook.com>
2025-10-30 23:26:42 +0300

    FEAT - oop basics 10

--- 048_oop_basics/main.c
+++ 048_oop_basics/main.c
@@ -41,11 +41,62 @@ void shape_print(Shape *s)
 
 // -- subtypes
 
-// circle
+
+/*----------- circle --------------*/
 typedef struct
 {
-    /* data */
-};
+    Shape base;
+    double radius;
+}Circle;
+
+// methods of circle
+double circle_get_area(Shape *s)
+{
+    Circle *c = (Circle*)s;
+    return (3.14f * c->radius * c->radius);
+}
+
+double circle_get_perimeter(Shape *s)
+{
+    Circle *c = (Circle*)s;
+    return (2 * 3.14f * c->radius);
+}
+
+void circle_print(Shape *s)
+{
+    Circle *c = (Circle*)s;
+    printf("[Circle %s] r = %.2f area = %.2f peri=%.2f\n ",c->base.name,c->radius,,circle_get_area(s),circle_get_perimeter(s));
+}
+
+// circle vtable (one of copy)
+const ShapeVTable circle_vtable = 
+{
+    .get_area = circle_get_area,
+    .get_perimeter = circle_get_perimeter,
+    .print = circle_print
+};  
+
+
+// circle maker
+void circle_init(Circle *c, const char *name, double r)
+{
+    c->base.vptr = &circle_vtable;
+    c->base.name = name;
+    c->radius = r;
+}
+
+/*----------- square --------------*/
+typedef struct 
+{
+    Shape base;
+    double side;
+}Square;
+
+
+
+
+
+
 
 
 int main(void)
